08632ea78aced7d39a43cc15611df91cebd4ec43
LanaSharabati <lanasharabati99@gmail.com>
2022-10-12 19:20:11 +0300

    aasigment 1
    to improve

--- Relevant Articles.py
+++ Relevant Articles.py
@@ -1,14 +1,51 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 10 16:18:15 2022
-
-@author: Asus
-"""
 import glob
 import os
+'''
+to work in make user enter number of keyword find all the
+keyword then make the user enter article number
+update irrelevant words
+update all the code classes and add documentation
+'''
+def article_name_list():
+    os.chdir('articles') #have problem here solve it!!!
+    article_Files = glob.glob('*.txt')
+    return article_Files
+    
+def read_File(article_Files):
+    input_texts =[]
+    for i in range(len(article_Files)):
+        print(i+1,article_Files[i])
+        file = open(article_Files[i])
+        input_text = file.read()
+        input_text = input_text.lower()
+        input_texts.append(input_text)
+    return input_texts
+
+
+def word_lists(input_text):
+    irrelevant_words = {".", "\n", 'the',"what", "and",
+    "is", "was", "he", "at", "to", "for", "can",'of', 'in'
+    , 'also', 'after',"a",'are','we','as', 'our','on', 'that','it','by',
+    'have', 'this', 'but','been', 'be', 'has','their','with', 'or','will'
+    ,'from','they', 'you','not','most','her', 'she','his','--','mr','told',
+    '-','mr.','were','had','how','while','said','had','many','its','i','my'
+    ,'an','who','about','would', 'so','which','says','when', 'just','us','want'
+    ,'more','get','use','some','than','then','made',
+    'because','such',
+    'him','go',
+    'over'
+    'me'}
+    
+    word_list = input_text.split(" ")
+    for iw in irrelevant_words:
+        for j in range(len(word_list)):
+            if word_list[j] == iw:
+                word_list[j] = ""
+        # input_text = input_text.replace(iw, "")
+    return word_list
+
 
 def histogram(word_list):
-    
    word_histogram = {
    }
    for word in word_list:
@@ -17,28 +54,22 @@ def histogram(word_list):
        else:
            word_histogram[word] = word_histogram[word] + 1
    word_histogram.pop("")
-   return(word_histogram)
+   return word_histogram
 
-def list_(input_text):
-    irrelevant_words = {".", "," , "\n", 'the', " a ", " and ",
-    " is ", " was ", " he ", " at ", " to ", " for ", " can ",
-    "of","that","in","by","on","said","his","will","are",
-    '"'}
-    for iw in irrelevant_words:
-        input_text = input_text.replace(iw, " ")
-        word_list = input_text.split(" ")
-    return word_list
 
-def keywords(my_dict):####
+def article_keyword(my_dict):
     x=list(my_dict.values())
     x.sort(reverse=True)
-    x=x[:2]
-    y = []
+    x=x[:4]
+    x = [*set(x)]
+    keywords = []
     for i in x:
+       
         for j in my_dict.keys():
             if(my_dict[j]==i):
-                y.append(str(j))
-    return(y)
+                keywords.append(j)
+    return keywords
+
 
 def relavent(keyword1,keyword2,file):
     c=0
@@ -47,53 +78,53 @@ def relavent(keyword1,keyword2,file):
             if keyword1[i] == keyword2[j]:
                 c = c+1
     return [c,file]
+
+
+def the_Most_Intersection(keyword1,keyword2,files):##problem!!   
+    r=[]
+    for i in range(len(keyword2)):
+        re = relavent(keyword1,keyword2[i],files[i])
+        r.append(re)
+    
+    q=[]
+    maxi = max(r)
+    if maxi[0] != 0:
+       for i in range(len(r)):
+           if maxi[0] == r[i][0]:
+               q.append(r[i][1]) 
+       return q
+    else:
+       return("no relative article!!!!")
         
-        
+   
     
-    
-os.chdir('articles')    
-"'Article0.txt'"    
-h2 =[]
-m2 =[]
-file_read = input("enter file name") 
-# os.chdir('articles')
-
-file = open(file_read)
-input_text = file.read()
-word_list = list_(input_text)
-a = histogram(word_list) 
-h2.append(a)
-k= keywords(a)
+      
 
 
-  
-h =[]
-m =[]
-r=[]
-# os.chdir('articles') 
-my_files = glob.glob('*.txt')
+#convert to classes
+list_of_article = article_name_list()
+article_texts = read_File(list_of_article)
+main_article_number = int(input("enter the number of article you want to find it relevants"))
+main_article = article_texts[main_article_number-1]
+main_article_name = list_of_article[main_article_number-1]
+article_texts.remove(main_article)
+list_of_article.remove(main_article_name)
 
-my_files.remove(file_read)
-print(my_files)
-for i in range(len(my_files)):
-    if my_files[i] != file_read:
-        file = open(my_files[i])
-        input_text2 = file.read()
-        word_list = list_(input_text2)
-        a = histogram(word_list) 
-        h.append(a)
-        k= keywords(a)
-        m.append(k)
+
+
+
+main_article_dictionary = histogram(word_lists(main_article))
+main_keyword = article_keyword(main_article_dictionary)
+# find the histograme for each file text
+keywords = []
+for i in range(len(article_texts)):
+    article_dictionary = histogram(word_lists(article_texts[i]))
+    keyword = article_keyword(article_dictionary)
+    keywords.append(keyword)
     
 
-for i in range(len(m)):
-    re = relavent(k,m[i],my_files[i])
-    r.append(re)
+ 
     
-q=[]
-maxi = max(r)
-for i in range(len(r)):
-    if maxi[0] == r[i][0]:
-        q.append(r[i][1])   
-    
-print("articles with the most intersection",q)   
+print("articles with the most intersection",the_Most_Intersection(main_keyword,keywords,list_of_article)   )    
+
+ 
